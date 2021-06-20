85c9c8d2da592bbae19817f1a7d73b9d5bf0d094
MechEngMalik <mecheng.malik93@gmail.com>
2021-06-20 16:38:24 +0300

    f

---
+++ script.js
@@ -0,0 +1,32 @@
+
+let userName = prompt('what is your name');
+ console.log(userName);
+
+
+var userAge = 35;
+var userAge = 25;
+console.log(userAge);
+
+
+grade = 100;
+var grade;
+console.log(grade);
+
+
+let userName = 'shihab';
+
+console.log(userName);
+
+// mark = 90;
+// let mark;
+// console.log(mark);
+
+
+let str1 = 'shihab ';
+let str2 = 'eshtaiwi';
+console.log(str1 + str2);
+
+let num1 = 10;
+let num2 = '100';
+let num3 = num1 + num2;
+console.log(typeof num3);
